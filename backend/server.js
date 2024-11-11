89a423691b35577404f17adb0a6a3e137a32aa87
Lucifer0411 <sanjaypawar04112001@gmail.com>
2024-11-11 14:15:12 +0530

    cors config

--- backend/server.js
+++ backend/server.js
@@ -17,30 +17,23 @@ const app=express();
 //     credentials: true, // Include if you need cookies to be sent
 //   }));
 
-  const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-      'PUT',
-      'DELETE'
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-  
-app.use(cors(corsOpts));
-
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
+
+
 
 // const __dirname=path.resolve();
 // app.use(express.static(path.join(__dirname, 'backend','uploads','images')))
 dotenv.config();
 const port=process.env.PORT
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 
 app.use('/api/users',userRouter)
