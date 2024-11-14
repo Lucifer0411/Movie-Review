0255d8a2d454c98c408630f0a91dee326b093c62
Lucifer0411 <sanjaypawar04112001@gmail.com>
2024-11-14 11:50:27 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -23,19 +23,26 @@ app.use(express.static(path.join(__dirname,'uploads','images')))
 dotenv.config();
 const port=process.env.PORT
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 // app.use(cors({
 //     origin: '*', // Allow only your frontend origin
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //     credentials: true, // Include if you need cookies to be sent
 //   }));
+
+app.use(cors({
+  origin: '*', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow necessary methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow headers used in requests, like Content-Type and Authorization
+  credentials: true, // If you need cookies or other credentials
+}));
 
 
 app.use('/api/users',userRouter)
